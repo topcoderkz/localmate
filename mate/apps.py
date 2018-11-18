68d9b1bd8926b5e7ed362ad99dba11e837441477
Akerke Kozhatay <akerke.kozhatay@MacBook-Air-Akerke.local>
2018-11-18 22:00:02 +0900

    first commit

---
+++ mate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MateConfig(AppConfig):
+    name = 'mate'
---
+++ mate/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    is_mate = models.BooleanField(default=False)
+    is_guest = models.BooleanField(default=False)
+
+
+class Mate(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    skills = models.ManyToManyField(Skill, related_name='possessed_mates')
+
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200)
+    proficiency = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
