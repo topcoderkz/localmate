341b21c7e5d143b43a00c641ce29bbb4ca10091d
Zumrat Kochshegulov <topcoderkz@gmail.com>
2018-12-16 21:37:44 +0900

    small change for testing

--- mate/admin.py
+++ mate/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Mate, Skill
+from .models import User, Mate, Skill
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Mate)
 admin.site.register(Skill)
